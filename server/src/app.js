bfaf6a8c97e58553b140019883fcd2a3d2b0fafd
Gaurav Kumar <158594646+gaurav95-alt@users.noreply.github.com>
2026-09-09 23:38:34 +0530

    feat: configure JustSPAI Express application

---
+++ server/src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const healthRoutes = require('./routes/healthRoutes');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+const env = require('./config/env');
+
+const app = express();
+
+app.disable('x-powered-by');
+
+app.use(cors({
+  origin: env.clientOrigin === '*' ? true : env.clientOrigin,
+  credentials: env.clientOrigin !== '*',
+}));
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: true, limit: '1mb' }));
+
+app.get('/', (req, res) => {
+  res.json({ success: true, message: 'JustSPAI API is running' });
+});
+
+app.use('/api/health', healthRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = app;
