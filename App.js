09ce346d54acb4b4ade76d2562f981a83a81eea5
Gaurav Kumar <158594646+gaurav95-alt@users.noreply.github.com>
2026-09-09 23:34:17 +0530

    Connect JustSPAI app navigation

---
+++ App.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import AppNavigator from './src/navigation/AppNavigator';
+export default function App(){return <><StatusBar style="light" backgroundColor="#0A2540"/><AppNavigator/></>}
